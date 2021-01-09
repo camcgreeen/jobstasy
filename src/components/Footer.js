5c2ae6730c468e57c41e6bf51e2f03f653196a39
Cam Green <c.c.green@outlook.com>
2021-01-09 23:02:05 +0000

    complete responsive pass for /jobs page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,27 +10,31 @@ class Footer extends React.Component {
         <Link to="/jobs">
           <img
             className="footer__img"
-            src="https://svgshare.com/i/Spt.svg"
+            src="https://svgshare.com/i/SxA.svg"
             alt=""
           />
         </Link>
         <ul className="footer__links">
-          <li>Contact me</li>
+          {/* <li className="footer__links__contact">Contact me</li> */}
           <li>
             <a
               href="mailto:c.c.green@outlook.com"
-              style={{ color: "white", fontSize: "1.25rem" }}
+              style={{
+                // color: "white",
+                fontSize: "1.55rem",
+                textDecoration: "none",
+              }}
             >
               c.c.green@outlook.com
             </a>
           </li>
           <br />
           <br />
-
-          <Link to="/jobs" style={{ textDecoration: "none" }}>
-            <li>Jobs</li>
-          </Link>
-
+          <li>
+            <Link to="/jobs" style={{ textDecoration: "none" }}>
+              Jobs
+            </Link>
+          </li>
           <li>
             <Link to="/likes" style={{ textDecoration: "none" }}>
               My Likes
@@ -43,7 +47,7 @@ class Footer extends React.Component {
               target="_blank"
               rel="noreferrer noopener"
             >
-              GitHub project repo
+              GitHub Source
             </a>
           </li>
         </ul>
