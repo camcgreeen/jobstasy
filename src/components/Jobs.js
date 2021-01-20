2c1909531f5e15b2e2bbcb07c03d667e781dafd7
Cam Green <c.c.green@outlook.com>
2021-01-20 22:48:58 +0000

    refactor Jobs component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -7,7 +7,13 @@ import JobList from "./JobList";
 import Pagination from "./Pagination";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import { disableRightMiddleClick } from "../utilities/helper";
+import {
+  disableRightMiddleClick,
+  getRandom,
+  convertJobIdToSeed,
+  convertToSalary,
+  urlRegex,
+} from "../utilities/helper";
 import "./main.scss";
 import "./Jobs.scss";
 const firebase = require("firebase");
@@ -152,70 +158,36 @@ class Jobs extends React.Component {
         `/positions.json?description=${this.state.description}&location=${this.state.location}`
       );
     } catch (err) {
-      console.log(err);
       jobs = [];
       await this.setState({ jobs, noJobsFound: true });
       return;
     }
     jobs = this.addAttributes(jobs.data);
     jobs = this.applyFilters(jobs);
-    console.log(jobs);
+
     await this.setState({
       currentPage: 1,
       jobs,
       noJobsFound: jobs.length > 0 ? false : true,
     });
-    // document.getElementById("scroll-to").scrollIntoView({ behavior: "smooth" });
-    // this.scrollToContent();
-    // this.scrollDiv.current.scrollIntoView({ behavior: "smooth" });
-  };
-  // THIS WILL NEED REVISION BEFORE DEPLOYMENT
-  // USING CORS WORK-AROUND
-  getDefaultJobs = async () => {
-    // axios("/positions.json").then((response) => {
-    //   // console.log(response.data);
-    //   return response.data;
-    // });
-    const response = await axios("/positions.json");
-    return response.data;
-  };
-  getRandom = (seed) => {
-    const x = Math.sin(seed++) * 10000;
-    return x - Math.floor(x);
-  };
-  convertJobIdToSeed = (id) => {
-    let result = 0;
-    id.split("").forEach((char) => (result += char.charCodeAt(0)));
-    return result;
-  };
-  convertToSalary = (random) => {
-    // generate salary between 25k and 75k
-    // const min = Math.floor(25 + randomNum * 50);
-    // const max = min + 5;
-    // return [min * 1000, max * 1000];
-    return Math.floor(45 + random * 15) * 1000;
   };
   addAttributes = (jobs) => {
     jobs = this.addSalary(jobs);
     jobs = this.addApplicants(jobs);
-    // jobs = this.addApplyBefore(jobs);
-    // jobs = this.addTimezones(jobs);
     return jobs;
   };
   generatePersistentSalary = (jobId) => {
-    const jobSeed = this.convertJobIdToSeed(jobId);
-    const random = this.getRandom(jobSeed);
-    return this.convertToSalary(random);
+    const jobSeed = convertJobIdToSeed(jobId);
+    const random = getRandom(jobSeed);
+    return convertToSalary(random);
   };
   generatePersistentApplicantNumber = (jobId) => {
-    const jobSeed = this.convertJobIdToSeed(jobId);
-    const random = this.getRandom(jobSeed);
+    const jobSeed = convertJobIdToSeed(jobId);
+    const random = getRandom(jobSeed);
     return Math.floor(4 + random * 58);
   };
   addSalary = (jobs) => {
     jobs.forEach((job) => {
-      // const min = 35000 + this.getRandomInt(10) * 1000;
-      // const max = min + this.getRandomInt(10) * 1000;
       const min = this.generatePersistentSalary(job.id);
       const max = min + 5000;
       job.salary_min = min;
@@ -239,61 +211,17 @@ class Jobs extends React.Component {
     });
     return jobs;
   };
-  // addTimezones = (jobs) => {
-  //   let timezones = ["GMT", "GMT + 1", "GMT + 2"];
-  //   // jobs.forEach((job) => {
-  //   //   job.timezones = timezones;
-  //   // });
-  //   // return jobs;
-
-  //   jobs.forEach((job) => {
-  //     // job.timezones = ["GMT", "GMT + 1", "GMT + 2"];
-  //     job.timezones = "GMT, GMT + 1, GMT + 2";
-  //   });
-  //   return jobs;
-  // };
-  searchJobs = async (query) => {
-    // console.log(query);
-    // const response = await fetch(
-    //   `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?search=${query}`
-    // );
-    // const response = await fetch(`/positions.json?search=${query}`);
-    // return await response;
-    axios(`/positions.json?search=${query}`).then((response) => {
-      // console.log(response);
-    });
-  };
-  // checkCompanyUrlExists = (jobs) => {
-  //   let result = [];
-  //   let count = 0;
-  //   jobs.forEach((job) => {
-  //     if (
-  //       job.company_url !== null &&
-  //       !result.includes(job.company) &&
-  //       result.length < 3
-  //     ) {
-  //       result.push(job);
-  //       count++;
-  //     }
-  //   });
-  //   // console.log(result);
-  //   return result;
-  // };
   checkCompanyUrlExists = (jobs) => {
     let result = [];
-    const regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
+    const regex = urlRegex;
     jobs.forEach((job) => {
       let companyAlreadyInList = false;
-      result.every(
-        (jobInList) => {
-          if (jobInList.company === job.company) {
-            companyAlreadyInList = true;
-            // console.log(job.company + " was already in list");
-            return;
-          }
+      result.every((jobInList) => {
+        if (jobInList.company === job.company) {
+          companyAlreadyInList = true;
+          return;
         }
-        // (companyAlreadyInList = jobInList.company === job.company)
-      );
+      });
       if (
         job.company_url !== null &&
         regex.test(job.company_url) &&
@@ -320,8 +248,6 @@ class Jobs extends React.Component {
     }
   };
   filterJobsByCompany = (jobs, searchTerms) => {
-    console.log("searchTerms = ", searchTerms);
-    console.log("typeof searchTerms", typeof searchTerms);
     let formattedSearchTerms = [...searchTerms];
     for (let i = 0; i < formattedSearchTerms.length; i++) {
       formattedSearchTerms[i] = formattedSearchTerms[i].split(" ");
@@ -354,18 +280,6 @@ class Jobs extends React.Component {
     return jobs.filter((job) => job.type === "Full Time");
   };
   applyFilters = (jobs) => {
-    // let firstFilter = false;
-    console.log("applying filters to ", jobs);
-    // if (
-    //   this.state.sort === "most recent" &&
-    //   JSON.stringify(this.state.salaryValue) === JSON.stringify([0, 150000]) &&
-    //   this.state.fullTimeOnly === false &&
-    //   JSON.stringify(this.state.companyTags) === JSON.stringify([])
-    // ) {
-    //   console.log("SOMETHING NEW");
-    //   // const originalJobs = [...this.state.jobs];
-    //   return [...this.state.prevJobs];
-    // } else {
     let filteredJobs = [...jobs];
     filteredJobs = this.sortJobs(filteredJobs, this.state.sort);
     if (this.state.companyTags.length > 0) {
@@ -382,14 +296,7 @@ class Jobs extends React.Component {
     filteredJobs = this.state.fullTimeOnly
       ? this.filterJobsByFullTime(filteredJobs)
       : filteredJobs;
-    // this.setState({ filteredJobs }, () => {
-    //   this.toggleFilters();
-    //   this.toggleNoScroll();
-    //   this.props.updateFilterState([this.state.inputSort, this.state.inputSalaryValue, this.state.inputFullTimeOnly, this.state.companyTags]);
-    //   console.log(filteredJobs);
-    // });
     return filteredJobs;
-    // }
   };
 }
 
--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -22,3 +22,20 @@ export const disableRightMiddleClick = () => {
     }
   });
 };
+
+export const getRandom = (seed) => {
+  const x = Math.sin(seed++) * 10000;
+  return x - Math.floor(x);
+};
+
+export const convertJobIdToSeed = (id) => {
+  let result = 0;
+  id.split("").forEach((char) => (result += char.charCodeAt(0)));
+  return result;
+};
+
+export const convertToSalary = (random) => {
+  return Math.floor(45 + random * 15) * 1000;
+};
+
+export const urlRegex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
