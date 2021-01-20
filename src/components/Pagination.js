71bb8fcb078bc516c5c8d9f093fb7eca549e8373
Cam Green <c.c.green@outlook.com>
2021-01-20 23:30:20 +0000

    refactor Pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -9,7 +9,6 @@ class Pagination extends React.Component {
 
   render() {
     let pageNumbers = [];
-
     for (
       let i = 1;
       i <= Math.ceil(this.props.totalJobs / this.props.jobsPerPage);
@@ -17,15 +16,12 @@ class Pagination extends React.Component {
     ) {
       pageNumbers.push(i);
     }
-    // console.log(this.props.)
-    console.log(pageNumbers);
     return (
       <nav className="nav-pagination">
         <ul className="pagination">
           <li
             key={-1}
             className="page-item"
-            // onClick={() => this.props.paginate(this.props.currentPage + 1)}
             onClick={() => this.props.paginate(1)}
             className="page-link page-link--arrow-2"
           >
@@ -34,7 +30,6 @@ class Pagination extends React.Component {
           <li
             key={0}
             className="page-item"
-            // onClick={() => this.props.paginate(this.props.currentPage - 1)}
             onClick={() =>
               this.props.currentPage - 1 > 0 &&
               this.props.paginate(this.props.currentPage - 1)
@@ -54,22 +49,12 @@ class Pagination extends React.Component {
                   : "page-link"
               }
             >
-              {/* <a
-                onClick={() => this.props.paginate(number)}
-                className={
-                  this.props.currentPage === number
-                    ? "page-link active"
-                    : "page-link"
-                }
-              > */}
               {number}
-              {/* </a> */}
             </li>
           ))}
           <li
             key={pageNumbers + 1}
             className="page-item"
-            // onClick={() => this.props.paginate(this.props.currentPage + 1)}
             onClick={() =>
               this.props.currentPage + 1 < pageNumbers.length + 1 &&
               this.props.paginate(this.props.currentPage + 1)
@@ -81,7 +66,6 @@ class Pagination extends React.Component {
           <li
             key={pageNumbers + 2}
             className="page-item"
-            // onClick={() => this.props.paginate(this.props.currentPage + 1)}
             onClick={() => this.props.paginate(pageNumbers.length)}
             className="page-link page-link--arrow-2"
           >
@@ -92,27 +76,5 @@ class Pagination extends React.Component {
     );
   }
 }
-
-// const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-//   const pageNumbers = [];
-
-//   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-//     pageNumbers.push(i);
-//   }
-
-//   return (
-//     <nav>
-//       <ul className="pagination">
-//         {pageNumbers.map((number) => (
-//           <li key={number} className="page-item">
-//             <a onClick={() => paginate(number)} href="!#" className="page-link">
-//               {number}
-//             </a>
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// };
 
 export default Pagination;
