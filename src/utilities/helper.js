80a498e83ebd20c9568205f094456437d6f4a678
Cam Green <c.c.green@outlook.com>
2021-02-13 14:55:59 +0000

    update email address

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -40,7 +40,7 @@ export const convertToSalary = (random) => {
 
 export const urlRegex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
 
-export const emailAddress = "c.c.green@outlook.com";
+export const emailAddress = "hello@camgreen.works";
 
 export const sortJobs = (jobs, sortBy) => {
   switch (sortBy) {
